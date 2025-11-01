2af25dacf016f3bd9a99d46c574ff62c8e20e630
wyl <wyl@wyl>
2025-11-01 15:36:47 +0800

    chore

--- config.sh
+++ config.sh
@@ -4,6 +4,6 @@ mv ~/.config/nvim ~/.config/nvim.bak
 mv ~/.vimrc ~/.vimrc.bak
 mv ~/.tmux.conf ~/.tmux.conf.bak
 ln -s ~/configs/config.fish ~/.config/fish/config.fish
-ln -s ~/configs/config.nvim ~/.config/nvim
+ln -s ~/configs/nvim ~/.config/nvim
 ln -s ~/configs/tmux.conf ~/.tmux.conf
 ln -s ~/configs/vimrc ~/.vimrc
